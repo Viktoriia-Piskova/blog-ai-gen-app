b7ce07f4c9ede182fff1fbd2c79765701e766f36
piskova <v.piskova@beetroot.se>
2024-07-22 18:23:42 +0300

    Add auth0 for authentication

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>
-    <h1>From index js</h1>
-   </div>
-    ;
+  return (
+    <div>
+      <h1>From index js</h1>
+      <div>
+        <Link href="/api/auth/login">Login</Link>
+      </div>
+    </div>
+  );
 }
