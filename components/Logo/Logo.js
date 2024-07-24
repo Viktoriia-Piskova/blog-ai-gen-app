a198ff56a5e890b03b6f7ba72777f071b56fb501
piskova <v.piskova@beetroot.se>
2024-07-24 19:08:14 +0300

    Add Google Fonts

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -2,8 +2,13 @@ import { faBrain } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Logo = () => {
-  return <div className="text-3xl text-center py-4 ">
-    <span className="pr-1">AI Blog Gen</span>
-  <FontAwesomeIcon icon={faBrain} className="text-3xl text-center text-slate-400"/>
-  </div>;
+  return (
+    <div className="text-3xl text-center py-4 ">
+      <span className="pr-1 font-heading">AI Blog Gen</span>
+      <FontAwesomeIcon
+        icon={faBrain}
+        className="text-3xl text-center text-slate-400"
+      />
+    </div>
+  );
 };
