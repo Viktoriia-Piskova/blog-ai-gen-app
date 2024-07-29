0acc353ed086bfcbc214354e04d629d113819085
piskova <v.piskova@beetroot.se>
2024-07-29 18:06:44 +0300

    Base setup for AI instrument for text generation

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,3 +1,28 @@
-export default function handler(req, res) {
-    res.status(200).json({ name: 'generate post' })
+import { HfInference } from "@huggingface/inference";
+const apiKey = process.env.HF_TOKEN;
+const hf = new HfInference(apiKey);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const prompt = "Write a short text about home puppy for a 4 year old child";
+
+    try {
+      const result = await hf.textGeneration({
+        model: "gpt2", // You can use any other available model
+        inputs: prompt,
+        parameters: { max_new_tokens: 250 },
+      });
+
+      console.log(result);
+
+      res.status(200).json({ text: result.generated_text });
+      
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Error generating text" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
   }
+  //res.status(200).json({ name: "generate post" });
+}
