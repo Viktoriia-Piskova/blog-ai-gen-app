fe56faccf43a54f43a5eb0acd754e2f4351089d7
piskova <v.piskova@beetroot.se>
2024-08-01 18:55:41 +0300

    MongoDB: user tokens base setup

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -2,16 +2,27 @@ import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../components/AppLayout";
 
 export default function TokenPopup() {
+  const handleClick = async () => {
+    const response = await fetch("/api/addTokens", {
+      method: "POST",
+    });
+    const json = await response.json();
+    console.log(json);
+  };
+
   return (
     <div>
       <h1>From token popup</h1>
+      <button className="btn" onClick={handleClick}>
+        Add Tokens
+      </button>
     </div>
   );
 }
 
 TokenPopup.getLayout = function getLayout(page, pageProps) {
-  return <AppLayout {...pageProps}> {page} </AppLayout>
-}
+  return <AppLayout {...pageProps}> {page} </AppLayout>;
+};
 
 export const getServerSideProps = withPageAuthRequired(() => {
   return {
