94f68b07ccfd262b4bcc2baef45a07317ee03d6f
piskova <v.piskova@beetroot.se>
2024-08-02 18:22:00 +0300

    Load the post data for a post page

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,15 +1,53 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-
-export default function Post() {
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../../components/AppLayout";
+import { ObjectId } from "mongodb";
+import clientPromise from "../../lib/mongodb";
+export default function Post(props) {
+  console.log(props);
   return (
     <div>
-      <h1>From post page</h1>
+      <h1>{props.title}</h1>
+      <div>{props.postContent} </div>
+      <div>{props.metaDescription}</div>
     </div>
   );
 }
 
-export const getServerSideProps = withPageAuthRequired(() => {
-  return {
-    props: {},
-  };
+Post.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}> {page} </AppLayout>;
+};
+
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    const userSession = await getSession(ctx.req, ctx.res);
+    const client = await clientPromise;
+    const db = client.db("AIBlogGen");
+
+    const user = await db.collection("users").findOne({
+      auth0Id: userSession.user.sub,
+    });
+
+    const post = await db.collection("posts").findOne({
+      _id: new ObjectId(ctx.params.postId),
+      userId: user._id,
+    });
+
+    if (!post) {
+      return {
+        redirect: {
+          destination: "post/new",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {
+        postContent: post.postContent,
+        title: post.title,
+        metaDescription: post.metaDescription,
+        keywords: post.keywords,
+      },
+    };
+  },
 });
