f2ee03ee7d17ba15032c170e58f6b4b8ed26605a
piskova <v.piskova@beetroot.se>
2024-08-08 17:29:40 +0300

    Implement stripe webhooks

--- pages/api/addTokens.js
+++ pages/api/addTokens.js
@@ -1,5 +1,4 @@
 import { getSession } from "@auth0/nextjs-auth0";
-import clientPromise from "../../lib/mongodb";
 import stripeInit from "stripe";
 
 const stripe = stripeInit(process.env.STRIPE_SECRET_KEY);
@@ -22,26 +21,15 @@ export default async function handler(req, res) {
     line_items: lineItems,
     mode: "payment",
     success_url: `${protocol}${host}/success`,
-  });
-
-  const client = await clientPromise;
-  const db = client.db("AIBlogGen");
-  const userProfile = await db.collection("users").updateOne(
-    {
-      auth0Id: user.sub,
+    payment_intent_data: {
+      metadata: {
+        sub: user.sub
+      }
     },
-    {
-      $inc: {
-        availableTokens: 10,
-      },
-      $setOnInsert: {
-        auth0Id: user.sub,
-      },
-    },
-    {
-      upsert: true,
+    metadata: {
+      sub: user.sub
     }
-  );
+  });
 
   res.status(200).json({session: checkoutSession});
 }
