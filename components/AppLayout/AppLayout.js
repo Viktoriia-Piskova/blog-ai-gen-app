272aaa354a2a70851d8665ee38ef82558d8c8c93
piskova <v.piskova@beetroot.se>
2024-08-09 17:39:37 +0300

    Create posts context and retreive 5 latest posts

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -4,9 +4,23 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoins } from "@fortawesome/free-solid-svg-icons";
 import { Logo } from "../Logo";
+import { useContext, useEffect } from "react";
+import  PostsContext  from "../../context/postsContext";
 
-export const AppLayout = ({ children, availableTokens, posts, postId }) => {
+export const AppLayout = ({
+  children,
+  availableTokens,
+  posts: postsFromSSR,
+  postId,
+}) => {
   const { user } = useUser();
+
+  const { setPostsFromSSR, posts } = useContext(PostsContext);
+
+  useEffect(() => {
+    setPostsFromSSR(postsFromSSR);
+  }, [postsFromSSR, setPostsFromSSR]);
+
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col text-white overflow-hidden">
@@ -22,10 +36,17 @@ export const AppLayout = ({ children, availableTokens, posts, postId }) => {
         </div>
         <div className="flex-1 overflow-y-auto bg-gradient-to-b from-slate-800 to-cyan-800 px-4">
           {posts.map((post) => (
-            <Link key={post._id} href={`/post/${post._id}`} className={`py-1 border border-white/0 block text-ellipsis overflow-hidden whitespace-nowrap my-1 px-2 bg-white/10 cursor-pointer rounded-sm ${postId === post._id ? "bg-white/20 border-white" : ""}`}>
+            <Link
+              key={post._id}
+              href={`/post/${post._id}`}
+              className={`py-1 border border-white/0 block text-ellipsis overflow-hidden whitespace-nowrap my-1 px-2 bg-white/10 cursor-pointer rounded-sm ${
+                postId === post._id ? "bg-white/20 border-white" : ""
+              }`}
+            >
               {post.topic}
             </Link>
           ))}
+          <div className="hover:underline text-sm text-slate-400 text-center cursor-pointer mt-4">Load more posts</div>
         </div>
         <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2 ">
           {" "}
