81ffecdcb24b25be19c4a64e06ac7cb2d37e5674
piskova <v.piskova@beetroot.se>
2024-08-14 16:50:52 +0300

    Hide load more posts button and fix posts disappear

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -15,7 +15,7 @@ export const AppLayout = ({
 }) => {
   const { user } = useUser();
 
-  const { setPostsFromSSR, posts, getPosts } = useContext(PostsContext);
+  const { setPostsFromSSR, posts, getPosts, noMorePosts } = useContext(PostsContext);
 
   useEffect(() => {
     setPostsFromSSR(postsFromSSR);
@@ -46,6 +46,7 @@ export const AppLayout = ({
               {post.topic}
             </Link>
           ))}
+          {!noMorePosts &&
           <div
             onClick={() => {
               getPosts({lastPostDate: posts[posts.length - 1].created});
@@ -53,7 +54,7 @@ export const AppLayout = ({
             className="hover:underline text-sm text-slate-400 text-center cursor-pointer mt-4"
           >
             Load more posts
-          </div>
+          </div>}
         </div>
         <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2 ">
           {" "}
--- context/postsContext.js
+++ context/postsContext.js
@@ -6,9 +6,20 @@ export default PostsContext;
 
 export const PostsProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
+  const [noMorePosts, setNoMorePosts] = useState(false);
 
   const setPostsFromSSR = useCallback((postsFromSSR = []) => {
-    setPosts(postsFromSSR);
+    // setPosts(postsFromSSR);
+    setPosts((value) => {
+      const newPosts = [...value];
+      postsFromSSR.forEach((post) => {
+        const exists = newPosts.find((p) => p._id === post._id);
+        if (!exists) {
+          newPosts.push(post);
+        }
+      });
+      return newPosts;
+    });
   }, []);
 
   const getPosts = useCallback(async ({ lastPostDate }) => {
@@ -21,6 +32,9 @@ export const PostsProvider = ({ children }) => {
     });
     const json = await result.json();
     const postsResult = json.posts || [];
+    if(postsResult.length < 5) {
+      setNoMorePosts(true)
+    }
     setPosts((value) => {
       const newPosts = [...value];
       postsResult.forEach((post) => {
@@ -34,7 +48,7 @@ export const PostsProvider = ({ children }) => {
   }, []);
 
   return (
-    <PostsContext.Provider value={{ posts, setPostsFromSSR, getPosts }}>
+    <PostsContext.Provider value={{ posts, setPostsFromSSR, getPosts, noMorePosts }}>
       {children}
     </PostsContext.Provider>
   );
